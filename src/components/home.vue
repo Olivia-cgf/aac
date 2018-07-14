f1de08990011df6fb62c17f7de514fe8d0315609
architectUser <1756303507@qq.com>
2018-07-14 11:14:30 +0800

    Add files via upload

---
+++ src/components/home.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="home">
+
+  <el-menu :default-active="activeIndex2"  class="el-menu-demo" mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">最新发布</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">行政服务</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+        <el-menu-item index="2-4-2">选项2</el-menu-item>
+        <el-menu-item index="2-4-3">选项3</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    <el-menu-item index="4"><a href="" target="_blank">订单管理</a></el-menu-item>
+  </el-menu>
+ </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style lang="">
+  
+</style>
